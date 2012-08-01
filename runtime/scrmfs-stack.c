7021b20d7e5d3337e344f67b81dd892dd845fd7c
Adam Moody <moody20@llnl.gov>
2012-08-01 15:34:14 -0700

    typecast pointer to stack structure instead of int

--- runtime/scrmfs-stack.c
+++ runtime/scrmfs-stack.c
@@ -18,24 +18,23 @@ size_t scrmfs_stack_bytes(int size)
 /* intializes stack to record all entries as being free */
 void scrmfs_stack_init(void* start, int size)
 {
-  scrmfs_stack* stack = (int*) start;
+  scrmfs_stack* stack = (scrmfs_stack*) start;
   stack->size = size;
   stack->last = size;
 
   int i;
-  int* entries = (int*) ((char*) start + sizeof(scrmfs_stack));
+  int* entries = (int*) ((char*)start + sizeof(scrmfs_stack));
   for (i = 0; i < size; i++) {
     /* flip the order so low numbers are at the top
      * to make debugging easier */
     entries[i] = size-1 - i;
-    //entries[i] = i;
   }
 }
 
 /* pops one entry from stack and returns its value */
 int scrmfs_stack_pop(void* start)
 {
-  scrmfs_stack* stack = (int*) start;
+  scrmfs_stack* stack = (scrmfs_stack*) start;
 
   /* check that the stack isn't empty */
   if (stack->last > 0) {
@@ -47,7 +46,7 @@ int scrmfs_stack_pop(void* start)
     int idx = stack->last;
 
     /* return the value of this item */
-    int* entries = (int*) ((char*) start + sizeof(scrmfs_stack));
+    int* entries = (int*) ((char*)start + sizeof(scrmfs_stack));
     int value = entries[idx];
     return value;
   } else {
@@ -59,7 +58,7 @@ int scrmfs_stack_pop(void* start)
 /* pushes item onto free stack */
 void scrmfs_stack_push(void* start, int value)
 {
-  scrmfs_stack* stack = (int*) start;
+  scrmfs_stack* stack = (scrmfs_stack*) start;
 
   /* check that we have space to push item onto stack */
   if (stack->last < stack->size) {
@@ -67,7 +66,7 @@ void scrmfs_stack_push(void* start, int value)
     int idx = stack->last;
 
     /* place item on stack */
-    int* entries = (int*) ((char*) start + sizeof(scrmfs_stack));
+    int* entries = (int*) ((char*)start + sizeof(scrmfs_stack));
     entries[idx] = value;
 
     /* increment last pointer to point to next item */
