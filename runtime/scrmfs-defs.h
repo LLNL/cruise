a91681e7c4821e6d4fda45b5b072866b13fa0ac6
Raghunath Raja Chandrasekar <rajachan@cse.ohio-state.edu>
2012-08-16 13:33:56 -0700

    adding a quick readme

--- runtime/scrmfs-defs.h
+++ runtime/scrmfs-defs.h
@@ -6,9 +6,9 @@
 #define SCRMFS_MAX_FILEDESCS    ( SCRMFS_MAX_FILES )
 
 #define SCRMFS_MAX_FILENAME     ( 128 )
-#define SCRMFS_MAX_MEM          ( 1024 * 1024 * 1024 )
+#define SCRMFS_MAX_MEM          ( 1 * 1024 * 1024 * 1024 )
 
-#define SCRMFS_CHUNK_BITS       ( 20 )
+#define SCRMFS_CHUNK_BITS       ( 26 )
 #define SCRMFS_CHUNK_SIZE       ( 1 << SCRMFS_CHUNK_BITS )
 #define SCRMFS_CHUNK_MASK       ( SCRMFS_CHUNK_SIZE - 1 )
 #define SCRMFS_MAX_CHUNKS       ( SCRMFS_MAX_MEM >> SCRMFS_CHUNK_BITS )
