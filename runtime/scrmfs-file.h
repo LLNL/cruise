cb162e48566dd8d1353fec372e8bf28d39cd0900
Raghunath Rajachandrasekar <rajachan@cse.ohio-state.edu>
2012-08-29 12:07:28 -0400

    fix-merge step 3

--- runtime/scrmfs-file.h
+++ runtime/scrmfs-file.h
@@ -1,6 +1,13 @@
 #include "uthash.h"
 #include "scrmfs-defs.h"
 
+enum flock_enum
+{
+    UNLOCKED,
+    EX_LOCKED,
+    SH_LOCKED
+};
+
 typedef struct
 {
     int fd;
@@ -12,6 +19,8 @@ typedef struct
     off_t size;   /* current file size */
     off_t chunks; /* number of chunks currently allocated to file */
     off_t chunk_ids[SCRMFS_MAX_CHUNKS]; /* chunk offset location in the mem pool */
+    pthread_spinlock_t fspinlock;
+    enum flock_enum flock_status;
 } scrmfs_filemeta_t;
 
 
