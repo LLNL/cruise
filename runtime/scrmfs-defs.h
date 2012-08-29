5a6247864b822fe22104594ac7a10554c9ef9997
Raghunath Rajachandrasekar <rajachan@cse.ohio-state.edu>
2012-08-29 12:04:14 -0400

    fixing-merge step 2

--- runtime/scrmfs-defs.h
+++ runtime/scrmfs-defs.h
@@ -8,11 +8,11 @@
 #define SCRMFS_MAX_FILENAME     ( 128 )
 
 //#define SCRMFS_MAX_MEM          ( 1 * 1024 * 1024 * 1024 )
-#define SCRMFS_MAX_MEM          ( 128 * 1024 * 1024 )
+#define SCRMFS_MAX_MEM          ( 256 * 1024 * 1024 )
 
-#define SCRMFS_CHUNK_BITS       ( 12 )
+#define SCRMFS_CHUNK_BITS       ( 24 )
 #define SCRMFS_CHUNK_SIZE       ( 1 << SCRMFS_CHUNK_BITS )
 #define SCRMFS_CHUNK_MASK       ( SCRMFS_CHUNK_SIZE - 1 )
 #define SCRMFS_MAX_CHUNKS       ( SCRMFS_MAX_MEM >> SCRMFS_CHUNK_BITS )
 
-#define SCRMFS_SUPERBLOCK_KEY   ( 1234 )
+#define SCRMFS_SUPERBLOCK_KEY   ( 4321 )
