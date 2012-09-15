7c11cab8858a55c79c96f3d8f5c7fb24754b5dd7
Kathryn Mohror <kathryn@llnl.gov>
2012-09-15 08:48:01 -0700

    adding more tests

--- runtime/tests/test1.c
+++ runtime/tests/test1.c
@@ -2,19 +2,24 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
 
 #define UNUSEDFD 5000
 
 #define SCRMFS_STATUS
 #ifdef SCRMFS_STATUS
-    #define status(fmt, args... )  printf("STATUS: %d %s: "fmt, testnum, __func__, ##args)
+    #define status(fmt, args... )  printf("STATUS: %d %s, line %d: "fmt, testnum, \
+                                          __func__, __LINE__,##args)
 #else
     #define status(fmt, args... )
 #endif
 
 #define SCRMFS_ERROR
 #ifdef SCRMFS_ERROR
-    #define error(fmt, args... )  printf("ERROR: %d %s: "fmt, testnum, __func__, ##args)
+    #define error(fmt, args... )  printf("ERROR: %d %s, line %d: "fmt, testnum, \
+                                         __func__, __LINE__, ##args)
 #else
     #define error(fmt, args... )
 #endif
@@ -25,6 +30,10 @@
 
 #define TESTFAIL(f,a) ++testnum; f = a; \
                    if (f >= 0){ error("test %d failed\n", testnum); return -1;} \
+                   else {status("test %d succeeded\n", testnum);}
+
+#define TESTFAILERR(f,a,err) ++testnum; f = a; \
+                   if (f >= 0 || err != errno){ error("test %d failed\n", testnum); return -1;} \
                    else {status("test %d succeeded\n", testnum);}
 
 #define TESTSUCC(f,a) ++testnum; f = a; \
@@ -40,10 +49,14 @@ int main(int argc, char ** argv){
 
   CHECK(rc = test_open()); 
   CHECK(rc = test_close());
+  CHECK(rc = test_unlink());
   CHECK(rc = test_mkdir());
   CHECK(rc = test_rmdir());
+  //CHECK(rc = test_stat());
+  CHECK(rc = test_access());
   CHECK(rc = test_write());
-
+  //read
+  //seek
   return 0;
 }
 
@@ -81,10 +94,10 @@ int test_open(){
 }
 
 int test_close(){
-   
-    char afile[20] = "/tmp/file1.txt";
-    int fd;
-    int ret;
+   char afile[20] = "/tmp/file1.txt";
+   int fd;
+   int ret;
+
    /* close a file that does not exist
     * should fail */
    TESTFAIL(ret, close(UNUSEDFD));
@@ -94,10 +107,40 @@ int test_close(){
    TESTSUCC(fd, open(afile, O_CREAT));
    TESTSUCC(ret, close(fd));
 
+   unlink(afile);
 
    /* TODO: test that close of deleted file fails
     * not in here now because not implemented in library */
    return 1; 
+}
+
+
+int test_unlink(){
+   char afile[20] = "/tmp/file1.txt";
+   char adir[20] = "/tmp/somewhere";
+   int fd;
+   int ret;
+
+   /* try to remove non-existent file
+    * should fail */
+   TESTFAILERR(ret, unlink(afile), ENOENT);
+
+   /* try to remove an existing file
+    * we're not checking to see if the file is busy/open, so
+    * should succeed */
+   fd = open(afile, O_CREAT);
+   TESTSUCC(ret, unlink(afile));
+
+   close(fd);
+   unlink(afile);
+
+   /* try to remove a directory
+    * should fail */
+   mkdir(adir, S_IRWXU);
+   TESTFAILERR(ret, unlink(adir), EISDIR);
+   rmdir(adir);
+
+   return 1;
 }
 
 int test_mkdir(){
@@ -135,6 +178,42 @@ int test_rmdir(){
    return 1;
 }
 
+int test_stat(){
+   char afile[20] = "/tmp/afile";
+   int fd;
+   struct stat buf;
+
+   //TESTFAIL(fd, stat(afile, &buf));
+   
+   return 1;
+}
+
+int test_access(){
+   char afile[20] = "/tmp/afile";
+   int fd;
+
+   /* test if we can access a non-existent file
+    * should fail */
+   TESTFAIL(fd, access(afile, R_OK));
+   TESTFAIL(fd, access(afile, W_OK));
+   TESTFAIL(fd, access(afile, X_OK));
+   TESTFAIL(fd, access(afile, F_OK));
+
+   /* create the file, see if we can access it
+    * since we are not checking permissions at this time, all these
+    * should succeed */
+   fd = open(afile, O_CREAT);
+   TESTSUCC(fd, access(afile, F_OK));
+   TESTSUCC(fd, access(afile, R_OK));
+   TESTSUCC(fd, access(afile, W_OK));
+   TESTSUCC(fd, access(afile, X_OK));
+
+   close(fd);
+   unlink(afile);
+
+   return 1;
+}
+
 
 int test_write(){
    char afile[20] = "/tmp/afile";
@@ -152,7 +231,7 @@ int test_write(){
    TESTSUCC(fd, open(afile, O_CREAT));
    TESTSUCC(ret, write(fd, buf, count));
    close(fd);
-   unlink(fd);
+   unlink(afile);
 
 
    return 1;
