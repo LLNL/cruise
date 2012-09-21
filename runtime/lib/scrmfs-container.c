a7d2337362a4b899636dfbb331d6cfd8f1460fa3
Kathryn Mohror <kathryn@llnl.gov>
2012-09-21 10:17:00 -0700

    adding file for container support

---
+++ runtime/lib/scrmfs-container.c
@@ -0,0 +1,12 @@
+
+#include <container.h>
+
+static int scrmfs_container_init(cs_store_handle){
+   char info[100];
+   cs_store_handle_t handle;
+   cs_store_init(info, &handle);
+}
+
+static int scrmfs_container_finalize(){
+   
+}
