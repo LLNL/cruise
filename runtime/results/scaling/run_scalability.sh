351c55bb2f5268b704a89f1422c2a4c0059d163b
Kathryn Mohror <kathryn@llnl.gov>
2012-10-17 16:35:44 -0700

    updates for typos, and right compiler

--- runtime/results/scaling/run_scalability.sh
+++ runtime/results/scaling/run_scalability.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 #MOAB variabes
-#MSUB -l nodes=128
+#MSUB -l nodes=1
 #MSUB -l walltime=30:00
+#MSUB -A asccasc
 
 #configuration parameters\
-export PROCS_PER_NODE=8
-export MAX_NUM_NODES=16
+export PROCS_PER_NODE=16
+export MAX_NUM_NODES=1
 export TEST_MEMCPY_SRC=$PWD/test_memcpy.c
 export TEST_MOD_RAMDISK_SRC=$PWD/test_ramdisk.c
 export OUTPUT_DIR=$PWD/logs
-export CC=mpicc
+export CC=mpiicc
 export SCRMFS_INSTALL_DIR=../../install
 export CONTAINER_DIR=../../container
 export LD_LIBRARY_PATH=$CONTAINER_DIR/lib:$LD_LIBRARY_PATH
@@ -20,7 +21,13 @@ export POST_SCRMFS_FLAGS=`$SCRMFS_INSTALL_DIR/bin/scrmfs-config --post-ld-flags`
 
 export RUN_BENCHMARKS="ramdisk memcpy scrmfs " 
 export BENCHMARK_DIR=$PWD
-export BENCHMARK_PARAMS="104857600 5 0"
+#export BENCHMARK_PARAMS="104857600 5 0" # 100 MB
+export BENCHMARK_PARAMS="134217728 5 0" # 128 MB
+
+. /usr/local/tools/dotkit/init.sh
+use -q ic-11.1.046 
+
+srun -n $MAX_NUM_NODES -N $MAX_NUM_NODES hostname
 
 #build benchmarks
 for bench in `echo $RUN_BENCHMARKS`
@@ -29,7 +36,7 @@ do
     then
         #compile scrmfs-aware test_ramdisk
         echo "building test_$bench.."
-        $CC $PRE_SCRMFS_FLAGS -L$CONTAINER_DIR/lib -O3 -o test_$bench $BENCHMARK_DIR/test_$bench.c $POST_SCRMFS_FLAGS -lcontainer
+        $CC $PRE_SCRMFS_FLAGS -L$CONTAINER_DIR/lib -O3 -o test_$bench $BENCHMARK_DIR/test_ramdisk.c $POST_SCRMFS_FLAGS -lcontainer
     else
         #compile native benchmarks
         echo "building test_$bench.."
@@ -38,8 +45,6 @@ do
 
 done
 
-
-mkdir $OUTPUT_DIR
 #cleanup nodes before running
 #srun -n $MAX_NUM_NODES -N $MAX_NUM_NODES $IPC_CLEAUP
 
@@ -51,9 +56,11 @@ do
         for (( j=0; j<3; j++ ))
         do
             echo "Running test_$bench on $i nodes ($totprocs ranks); $PROCS_PER_NODE procs/node"
+            echo "srun -N $i -n $totprocs $PWD/test_$bench $BENCHMARK_PARAMS  > $OUTPUT_DIR/$bench-n$totprocs-N$i-iter$j"
             srun -N $i -n $totprocs $PWD/test_$bench $BENCHMARK_PARAMS  > $OUTPUT_DIR/$bench-n$totprocs-N$i-iter$j
             #cleanup all nodes
-            srun -n $MAX_NUM_NODES -N $MAX_NUM_NODES $IPC_CLEAUP
+            echo "srun -n $MAX_NUM_NODES -N $MAX_NUM_NODES $IPC_CLEANUP"
+            srun -n $MAX_NUM_NODES -N $MAX_NUM_NODES $IPC_CLEANUP
         done
     done
 done
